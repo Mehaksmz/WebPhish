111d5d694ac02d8bd974ea9d5612a927ee2587ea
Mehaksmz <ms2188@hw.ac.uk>
2026-02-07 00:13:58 +0400

    Baseline CNN implementation

---
+++ src/model/train.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+
+print(tf.config.list_physical_devices('GPU'))
+
+IMG_SIZE = (256, 256)
+BATCH_SIZE = 32
+
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "/home/mehak/image-dataset/train",
+    labels="inferred",
+    label_mode="binary",
+    color_mode="grayscale",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "/home/mehak/image-dataset/val",
+    labels="inferred",
+    label_mode="binary",
+    color_mode="grayscale",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+)
+
+model = Sequential([
+    tf.keras.layers.Rescaling(1./255, input_shape=(256, 256, 1)),
+
+    Conv2D(32, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Conv2D(128, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=3,
+    restore_best_weights=True
+)
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=20,
+    callbacks=[early_stopping]
+)
+
+model.save("cnn_phishing_model.keras")
