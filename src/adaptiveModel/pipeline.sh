8f6b5fc2628723dde2d6e2b12cf2d8fa22c0dd2f
Mehaksmz <ms2188@hw.ac.uk>
2026-02-08 22:26:36 +0400

    Update pipeline script

--- src/adaptiveModel/pipeline.sh
+++ src/adaptiveModel/pipeline.sh
@@ -1,2 +1,4 @@
 python src/adaptiveModel/build_metadata.py
-python src/adaptiveModel/build_metadata.py
+python src/adaptiveModel/split_dataset.py
+python src/adaptiveModel/train.py
+python src/adaptiveModel/test.py
